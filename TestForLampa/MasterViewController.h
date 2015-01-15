242d65ae429c4b744ac19dcc8e51d575bd04f951
Lana-Resquit <kristy@Mac-Pro-stillHitch.local>
2015-01-15 09:22:04 -0800

    Initial Commit

---
+++ TestForLampa/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  TestForLampa
+//
+//  Created by Kristy on 15.01.15.
+//  Copyright (c) 2015 Kristyna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
